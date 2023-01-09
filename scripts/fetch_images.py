4f99a3c3e41e168d7f28edea7ef564e0d579bbc2
sheetalc-virtusa <118749082+sheetalc-virtusa@users.noreply.github.com>
2023-01-09 13:47:04 +0000

    Update fetch_images.py

--- scripts/fetch_images.py
+++ scripts/fetch_images.py
@@ -1,7 +1,7 @@
 import re
 image = []
 pattern = re.compile("image:")
-with open ('deployment.yaml', 'rt') as myfile:    
+with open ('manifest.yaml', 'rt') as myfile:    
     for line in myfile:
         if pattern.search(line) != None:
             clean_data_1 = (line.replace('image:', '')).strip()
